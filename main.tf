e00935458c877a83dfc41dd8db008e327da1df63
davidcallner <33468352+davidcallner@users.noreply.github.com>
2021-01-19 14:26:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,7 @@ provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.eks.endpoint
     token                  = data.aws_eks_cluster_auth.eks.token
-    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority.0.data)
-    load_config_file       = false
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority.0.data)   
   }
 }
 
