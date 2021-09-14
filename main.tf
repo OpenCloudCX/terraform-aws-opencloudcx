0a106d3182a2c2cca81edbb6871015350832f3c8
Adam Norris <anorris@rivasolutionsinc.com>
2021-09-14 10:46:50 -0400

    Added kubernetes provider

--- main.tf
+++ main.tf
@@ -6,3 +6,9 @@ provider "helm" {
   }
 }
 
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks.endpoint
+  token                  = data.aws_eks_cluster_auth.eks.token
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority.0.data)
+}
