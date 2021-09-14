12bafca5242e2299a2847a3cece884ccd1f26ef8
Adam Norris <anorris@rivasolutionsinc.com>
2021-09-14 15:19:11 -0400

    Added namespaces and new ingress rules for reverse proxy

--- ingress.tf
+++ ingress.tf
@@ -3,7 +3,8 @@ resource "kubernetes_ingress" "jenkins_ingress" {
   wait_for_load_balancer = true
 
   metadata {
-    name = "jenkins"
+    name      = "jenkins"
+    namespace = "jenkins"
     annotations = {
       "kubernetes.io/ingress.class" = "nginx"
     }
@@ -31,7 +32,9 @@ resource "kubernetes_ingress" "spinnaker_ingress" {
   wait_for_load_balancer = true
 
   metadata {
-    name = "spinnaker"
+    name      = "spinnaker"
+    namespace = "spinnaker"
+
     annotations = {
       "kubernetes.io/ingress.class" = "nginx"
     }
@@ -45,7 +48,7 @@ resource "kubernetes_ingress" "spinnaker_ingress" {
         path {
           path = "/"
           backend {
-            service_name = "spinnaker"
+            service_name = "spin-deck"
             service_port = 9000
           }
         }
@@ -53,3 +56,94 @@ resource "kubernetes_ingress" "spinnaker_ingress" {
     }
   }
 }
+
+resource "kubernetes_ingress" "sonarqube_ingress" {
+
+  wait_for_load_balancer = true
+
+  metadata {
+    name      = "sonarqube"
+    namespace = "sonarqube"
+
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+    }
+  }
+  spec {
+    rule {
+
+      host = "sonarqube.riva-cicd-0044.local"
+
+      http {
+        path {
+          path = "/"
+          backend {
+            service_name = "sonarqube-sonarqube"
+            service_port = 9000
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress" "grafana_ingress" {
+
+  wait_for_load_balancer = true
+
+  metadata {
+    name      = "grafana"
+    namespace = "opencloudcx"
+
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+    }
+  }
+  spec {
+    rule {
+
+      host = "grafana.riva-cicd-0044.local"
+
+      http {
+        path {
+          path = "/"
+          backend {
+            service_name = "grafana"
+            service_port = 3000
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress" "anchore_ingress" {
+
+  wait_for_load_balancer = true
+
+  metadata {
+    name      = "anchore"
+    namespace = "anchore-engine"
+
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+    }
+  }
+  spec {
+    rule {
+
+      host = "anchore.riva-cicd-0044.local"
+
+      http {
+        path {
+          path = "/"
+          backend {
+            service_name = "anchore-engine-anchore-engine-api"
+            service_port = 8228
+          }
+        }
+      }
+    }
+  }
+}
+
