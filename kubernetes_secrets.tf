720b7fb247739154179f82fb601206a7de1d7f9d
Adam Norris <anorris@rivasolutionsinc.com>
2021-11-13 16:47:09 -0500

    Added kubernetes secret for codeserver

--- kubernetes_secrets.tf
+++ kubernetes_secrets.tf
@@ -1,6 +1,3 @@
-######################################################
-## Grafana secret
-
 resource "kubernetes_secret" "grafana_secret" {
   metadata {
     name      = "grafana-admin"
@@ -18,5 +15,24 @@ resource "kubernetes_secret" "grafana_secret" {
     aws_eks_cluster.eks,
     aws_eks_node_group.ng,
     kubernetes_namespace.opencloudcx,
+  ]
+}
+
+resource "kubernetes_secret" "codeserver_secret" {
+  metadata {
+    name      = "codeserver-password"
+    namespace = "develop"
+  }
+
+    data = {
+      password = var.code_server_secret
+    }
+
+  type = "kubernetes.io/basic-auth"
+
+  depends_on = [
+    aws_eks_cluster.eks,
+    aws_eks_node_group.ng,
+    kubernetes_namespace.develop,
   ]
 }
